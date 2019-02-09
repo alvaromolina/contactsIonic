baf416fdf6c3c1e31fef877756cddebf8e17e6fc
alvaromolinac@gmail.com <alvaromolinac@gmail.com>
2019-02-09 12:54:34 -0400

    Binding

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+interface Customer{
+  name: string;
+  lastName: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  titulo: string = 'Hello';
+  disabledButton: boolean = true;
+  contact: Customer = {name: "Juan", lastName: "Perez"};
 
 }
