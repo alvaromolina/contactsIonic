2515448ad376ffd0ec023231eaae91c1a44c2ea0
alvaromolinac@gmail.com <alvaromolinac@gmail.com>
2019-02-14 19:48:45 -0400

    contactos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
-interface Customer{
+
+interface Contact{
   name: string;
   lastName: string;
 }
@@ -12,7 +14,24 @@ interface Customer{
 })
 export class HomePage {
   titulo: string = 'Hello';
-  disabledButton: boolean = true;
-  contact: Customer = {name: "Juan", lastName: "Perez"};
+  disabledButton: boolean = false;
+  showButton: boolean = false;
 
+  contact: Contact = {name: "Juan", lastName: "Perez"};
+
+  contacts: Contact[] = [{name: "Juan", lastName: "Perez"}, {name: "Pedro", lastName: "Ramirez"}]
+  constructor(public alertController: AlertController){
+    
+  }
+
+  async showAlert(){
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
